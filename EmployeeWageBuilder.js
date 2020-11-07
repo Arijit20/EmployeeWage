c494ea963cbbf100a5b7439881f245626efec753
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-11-07 22:21:10 +0530

    [Arijit]Added : Method To Calculate Wage For A Month With 20 Working Days

--- EmployeeWageBuilder.js
+++ EmployeeWageBuilder.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck){
 switch (empCheck) {
@@ -17,7 +18,9 @@ switch (empCheck) {
 }
 }
 let empHrs = 0;
+for(let day = 0; day <NUM_OF_WORKING_DAYS; day++){
 let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Emp Wage : "+ empWage);
+console.log("Total Hours : "+ empHrs +" Emp Wage : "+ empWage);
